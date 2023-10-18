4d8bbcd7e4f3e5e17ac16cad1fa934d7b3e81190
Seth-msum <122572767+Seth-msum@users.noreply.github.com>
2023-10-17 22:54:41 -0500

    Update PrettyPrinter.java

--- assign4/pretty/PrettyPrinter.java
+++ assign4/pretty/PrettyPrinter.java
@@ -1,6 +1,67 @@
+package assign4.pretty ;
 
-public class PrettyPrinter extends ASTVisitor {}
+import assign4.visitor.* ;
+import assign4.parser.* ;
+
+import java.io.* ;
+
+public class PrettyPrinter extends ASTVisitor {
     //The pretty printer should apply stylistic formatting conventions
     //  to the input program, adjusting positioning and spacing. 
     // The formatted output should be saved in a file named "output.txt."
+
+    public Parser parser = null;
+
+    public PrettyPrinter(){
+
+        this.parser = parser;
+        visit(this.parser.cu);
+    }
+
+    public PrettyPrinter(){
+
+        visit(this.parser.cu);
+    }
+
+    ////////////////////////////////////////
+    //  Utility methods
+    ////////////////////////////////////////
+
+    void print(String s){
+
+        System.out.print(s);
+    }
+
+    void println(String s){
+
+        System.out.println(s);
+    }
+
+    void printSpace(){
+
+        System.out.print(" ");
+    }
+
+    int indent = 0;
+
+    void indentUp(){
+
+        indent++;
+    }
+
+    void indentDown(){
+
+        indent--;
+    }
+
+    void printIndent(){
+
+        String s = "";
+        for (int i=0; i<indent; i++){
+
+            s += " ";
+        }
+
+        print(s);
+    }
 }
